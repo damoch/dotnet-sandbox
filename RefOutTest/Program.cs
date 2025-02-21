dd53b817223ca7a8dffed760d77072617f224d3d
Damian Ochramowicz <damochramowicz260@gmail.com>
2025-02-21 17:27:07 +0100

    ref out sandbox

---
+++ RefOutTest/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace RefOutTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathOperations = new MathOperation();
+            int updateSubject = 5;
+            Console.WriteLine($"before update {updateSubject} ");
+            mathOperations.UpdateValue(ref updateSubject, 12);
+            Console.WriteLine($"after update {updateSubject} ");
+
+            double toSQ = 81;
+            double result;
+            mathOperations.CalculateSquareRoot(toSQ, out result);
+            Console.WriteLine($"sqrt of {toSQ} is {result}");
+            var tmp = result;
+
+            var powValue = 3;
+            mathOperations.CalculatePower(result, powValue, out result);
+            Console.WriteLine($"{tmp} to power of {powValue} is {result}");
+
+
+        }
+
+        class MathOperation
+        {
+            public void UpdateValue(ref int subject, int increment)
+            {
+                subject += increment;
+            }
+
+            public void CalculateSquareRoot(double input, out double result)
+            {
+                result = Math.Sqrt(input);
+            }
+
+            public void CalculatePower(in double baseNumber, in int exponent, out double result)
+            {
+                //baseNumber++; error - "in" parameters are readonly
+                result = Math.Pow(baseNumber, exponent);
+            }
+        }
+    }
+}
